d51f20f985493d9c93c823d4a6b546bf2412dc4d
SevenAJJY <yassine.elhajjy@gmail.com>
2022-07-20 22:48:53 +0100

    Added responsive section

--- js/main.js
+++ js/main.js
@@ -12,3 +12,28 @@ const headerBg = () => {
     })
 }
 headerBg();
+
+/************  nav *************/
+
+const navigation = () => {
+    const navToggler = document.querySelector('.js-nav-toggler');
+    const nav = document.querySelector('.js-nav');
+    const navItems = nav.querySelectorAll('li');
+
+    const navToggle = () => {
+        nav.classList.toggle('open');
+        navToggler.classList.toggle('active');
+    }
+
+    navToggler.addEventListener('click', navToggle);
+
+    navItems.forEach((li) => {
+        li.querySelector('a').addEventListener('click', () => {
+            if (window.innerWidth <= 767) {
+                navToggle();
+            }
+        });
+    });
+}
+
+navigation();
